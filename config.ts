787bc61f61726e069eb4b349031d607d0d3cc25c
choeqq <k.chola@op.pl>
2022-11-16 20:16:40 +0100

    /api/checkout route

--- config.ts
+++ config.ts
@@ -7,3 +7,5 @@ export const MAX_DONATION_IN_CENTS = parseInt(
   process.env.NEXT_PUBLIC_DONATION_IN_CENTS || "10000",
   10
 );
+
+export const STRIPE_API_KEY = process.env.STRIPE_API_KEY as string;
