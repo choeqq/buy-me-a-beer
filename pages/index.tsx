b4a28eb5165318b0b419f2b5d0a9f194b433579d
choeqq <k.chola@op.pl>
2022-11-16 20:07:03 +0100

    layout, styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,16 @@
 import Head from "next/head";
+import { useState } from "react";
+import { DONATION_IN_CENTS, MAX_DONATION_IN_CENTS } from "../config";
+import Image from "next/image";
+
+const presets = [1, 3, 5];
 
 export default function Home() {
+  const [error, setError] = useState(null);
+  const [quantity, setQuantity] = useState(1);
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+
   return (
     <div>
       <Head>
@@ -9,7 +19,71 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex"></main>
+      <main className="flex max-w-2xl m-auto">
+        <div className="flex-1">
+          <h2>Previous donations</h2>
+        </div>
+
+        <div>
+          <h1>Buy me a beer</h1>
+          {error && <div>{error}</div>}
+          <div className="flex items-center full-w mb-2">
+            <span className="mr-2">
+              <Image src="/beer.svg" width="100" height="100" alt="beer" />
+            </span>
+            <span className="mr-2">X</span>
+
+            {presets.map((preset) => {
+              return (
+                <button
+                  className="bg-blue-500 text-white px-4 py-2 rounded mr-2"
+                  key={preset}
+                  onClick={() => setQuantity(preset)}
+                >
+                  {preset}
+                </button>
+              );
+            })}
+
+            <input
+              type="number"
+              onChange={(e) => setQuantity(parseFloat(e.target.value))}
+              value={quantity}
+              min={1}
+              max={MAX_DONATION_IN_CENTS / DONATION_IN_CENTS}
+              className="shadow rounded w-full border border-blue-500 p-2"
+            />
+          </div>
+          <div className="mb-2 w-full">
+            <label className="block" htmlFor="name">
+              Name (Optional)
+            </label>
+            <input
+              className="shadow rounded w-full border border-blue-500 p-2"
+              type="text"
+              id="name"
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+              placeholder="Kacper"
+            />
+          </div>
+
+          <div className="mb-2 w-full">
+            <label htmlFor="message">Message (Optional)</label>
+            <textarea
+              className="shadow rounded w-full border border-blue-500 p-2"
+              id="message"
+              onChange={(e) => setMessage(e.target.value)}
+              value={message}
+              placeholder="Thank you"
+            />
+          </div>
+
+          <button className="bg-blue-500 rounded shadow px-4 py-2 text-white">
+            Donate ${quantity * (DONATION_IN_CENTS / 100)}
+          </button>
+        </div>
+      </main>
     </div>
   );
 }
